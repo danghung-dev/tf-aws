805bcdcf4b64d1cb34315340ca7eb44712773057
Dang Hung <hung.dang980@gmail.com>
2018-05-17 17:39:46 +0700

    add default tg

--- main.tf
+++ main.tf
@@ -150,9 +150,16 @@ resource "aws_lb_listener" "es" {
   protocol          = "HTTP"
 
   default_action {
-    target_group_arn = "${aws_lb_target_group.es.arn}"
+    target_group_arn = "${aws_lb_target_group.esdefault.arn}"
     type             = "forward"
   }
+}
+
+resource "aws_lb_target_group" "esdefault" {
+  name     = "tf-lb-tg-esdefault"
+  port     = 80
+  protocol = "HTTP"
+  vpc_id   = "${var.vpc_id}"
 }
 
 # resource "aws_lb_listener_rule" "es" {
